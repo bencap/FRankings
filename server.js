6533409d9beae9f10ac1454a2ae1e16ff78a02ef
Ben Capodanno <bencap@outlook.com>
2021-12-02 13:34:07 -0800

    add catch all route to handle refresh (router could only handle / route entrance

--- server.js
+++ server.js
@@ -10,8 +10,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(root, '/Angular/dist/FRatings-Angular')));
-console.log(path.join(root, '/Angular/dist/FRatings-Angular'));
-console.log(root);
 app.use('/api', routes);
+
+/* other routes defined before catch-all */
+app.get('/some-route', (req, res) => {
+  res.send('ok')
+})
+
+/* final catch-all route to index.html defined last */
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/Angular/dist/FRatings-Angular/index.html'));
+})
 
 app.listen(port, () => console.log(`API running on localhost:${port}`));
